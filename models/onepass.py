a65d3b58c25e501c1d9a28ab4c11fa1df7922916
annie <2459699580@qq.com>
2022-09-23 22:05:00 +0800

    one-stage | light backbone, dv-5.3M, params-1.37M, flops-0.1G.

--- models/onepass.py
+++ models/onepass.py
@@ -17,6 +17,7 @@ class OnePassASD(nn.Module):
         self.audio_tm = slib.TemporalModel(args.tm_base, 128, 128, 1)
         self.video_tm = slib.TemporalModel(args.tm_base, 128, 128, 1)
         self.avfea_tm = slib.TemporalModel(args.tm_base, 256, 256, 2)
+        self.drop_out = nn.Dropout(p=0.2)
         self.fully_connect = nn.Linear(256, 2)
     
     def forward(self, a, v):
